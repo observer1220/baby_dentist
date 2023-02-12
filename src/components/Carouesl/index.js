aa468db1e12040b8b92b10ecfb238d3563331a1a
observer1220 <observer1220@gmail.com>
2023-02-12 17:20:30 +0800

    update 2023-02-12 17:20:30

--- src/components/Carouesl/index.js
+++ src/components/Carouesl/index.js
@@ -1,7 +1,25 @@
 import React from "react";
 import { Carousel } from 'antd';
-import { slides } from './mock';
 import style from "./styles.module.css";
+
+import banner1 from '/static/img/index/banner1.jpg'
+import banner2 from '/static/img/index/banner2.jpg'
+import banner3 from '/static/img/index/banner3.jpg'
+
+const slides = [
+  {
+    id: 1,
+    url: banner1,
+  },
+  {
+    id: 2,
+    url: banner2,
+  },
+  {
+    id: 3,
+    url: banner3,
+  }
+]
 
 // Carousel的隱藏API請查閱React-Slick
 export default function CarouselComponent () {
--- src/pages/work_time/index.js
+++ src/pages/work_time/index.js
@@ -1,9 +1,156 @@
 import React from "react";
 import Layout from "@theme/Layout";
 import { Button, Divider, Space, Table } from 'antd';
-import { columns, BranchHuaSia, BranChingPu } from './mock';
 import style from './index.module.css';
 import { PhoneOutlined, WechatOutlined, GoogleOutlined } from '@ant-design/icons';
+
+const columns = [
+  {
+    title: '',
+    dataIndex: 'period_time',
+    key: 'period_time',
+    render: (text) => <strong>{text}</strong>,
+    align: 'center',
+  },
+  {
+    title: '一',
+    dataIndex: 'monday',
+    key: 'monday',
+    align: 'center',
+  },
+  {
+    title: '二',
+    dataIndex: 'tuesday',
+    key: 'tuesday',
+    align: 'center',
+  },
+  {
+    title: '三',
+    key: 'wednesday',
+    dataIndex: 'wednesday',
+    align: 'center',
+  },
+  {
+    title: '四',
+    dataIndex: 'thursday',
+    key: 'thursday',
+    align: 'center',
+  },
+  {
+    title: '五',
+    dataIndex: 'friday',
+    key: 'friday',
+    align: 'center',
+  },
+  {
+    title: '六',
+    dataIndex: 'saturday',
+    key: 'saturday',
+    align: 'center',
+  },
+]
+
+const BranchHuaSia = [
+  {
+    key: '1',
+    period_time: (
+      <>
+        <li>早診</li>
+        <li>09:30</li>
+        <li>11:30</li>
+      </>
+    ),
+    monday: '✅',
+    tuesday: '',
+    wednesday: '✅',
+    thursday: '',
+    friday: '✅',
+    saturday: '✅',
+  },
+  {
+    key: '2',
+    period_time: (
+      <>
+        <li>午診</li>
+        <li>14:30</li>
+        <li>17:00</li>
+      </>
+    ),
+    monday: '✅',
+    tuesday: '✅',
+    wednesday: '✅',
+    thursday: '✅',
+    friday: '✅',
+    saturday: '',
+  },
+  {
+    key: '3',
+    period_time: (
+      <>
+        <li>晚診</li>
+        <li>18:30</li>
+        <li>20:30</li>
+      </>
+    ),
+    monday: '✅',
+    tuesday: '✅',
+    wednesday: '✅',
+    thursday: '✅',
+    friday: '',
+    saturday: '',
+  },
+];
+
+const BranChingPu = [
+  {
+    key: '1',
+    period_time: (
+      <>
+        <li>早診</li>
+        <li>09:30</li>
+        <li>11:30</li>
+      </>
+    ),
+    monday: '',
+    tuesday: '',
+    wednesday: '',
+    thursday: '',
+    friday: '✅',
+    saturday: '✅',
+  },
+  {
+    key: '2',
+    period_time: (
+      <>
+        <li>午診</li>
+        <li>14:30</li>
+        <li>17:00</li>
+      </>
+    ),
+    monday: '✅',
+    tuesday: '✅',
+    wednesday: '✅',
+    thursday: '✅',
+    friday: '✅',
+    saturday: '',
+  },
+  {
+    key: '3',
+    period_time: (
+      <>
+        <li>晚診</li>
+        <li>18:30</li>
+        <li>20:30</li>
+      </>
+    ),
+    monday: '✅',
+    tuesday: '✅',
+    wednesday: '✅',
+    thursday: '✅',
+    friday: '',
+    saturday: '',
+  },
+]
 
 export default function ClinicInfo () {
   return (
