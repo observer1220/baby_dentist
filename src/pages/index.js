381a582e39bcdfa2b2583f75742b704886c41127
observer1220 <observer1220@gmail.com>
2023-08-17 16:33:15 +0800

    update 2023-08-17 16:33:15

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,6 +46,20 @@ export default function Home () {
           </div>
         </Col>
       </Row>
+      {/* 關於寶貝兔 */}
+      <Divider orientation="center">
+        <h2><strong>關於寶貝兔</strong></h2>
+      </Divider>
+      <Row>
+        <Col xs={0} md={8} lg={8}>
+          <img src="https://res.cloudinary.com/dbjxyxxlj/image/upload/v1692254760/logo_fgvpv2.png" />
+        </Col>
+        <Col xs={24} md={24} lg={16}>
+          <p style={{ margin: "50px" }}>
+            歡迎來到寶貝兔牙醫！我們由3位在「兒牙」、「植牙」與「美學」領域專精的博士團隊領軍，帶領10位技術精湛的牙醫師，一起致力於修復、寶貝您全家的牙齒。我們提供專業的治療技術和客製化的暖心服務，並以精雕細琢的牙齒美學，成就您對美麗笑容、自信人生的期待。
+          </p>
+        </Col>
+      </Row>
       {/* 醫師團隊 */}
       <Divider orientation="center">
         <h2><strong>寶貝兔醫師團隊</strong></h2>
